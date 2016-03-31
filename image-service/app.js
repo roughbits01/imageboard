fbefe7d50a3b2fe640b45933ad243a84a79d8344
unknown <a623909@FR-36VCY52.ww930.my-it-solutions.net>
2016-03-31 19:41:11 +0200

    color palette

--- image-service/app.js
+++ image-service/app.js
@@ -8,6 +8,7 @@ var logger = require('morgan');
 var multer  = require('multer');
 var sharp = require('sharp');
 var bodyParser = require('body-parser');
+var ColorThief = require('color-thief');
 var request = require('request');
 var async = require('async');
 var express = require('express');
@@ -31,7 +32,8 @@ app.get('/photos', function(req, res, next) {
   fs.readdir('uploads/original', (err, data) => {
     if (err) throw err;
     var images = data.map(function(e) {
-      return 'uploads/thumbnail/' + e;
+      var ext = path.extname(e);
+      return 'uploads/thumbnail/' + path.basename(e, ext) + '.jpg';
     });
     res.json(images);
   });
@@ -40,12 +42,16 @@ app.get('/photos', function(req, res, next) {
 /**
  * Prepare upload folder.
  */
- var allowedMime = [ "image/jpeg", "image/png" ];
+var allowedMime = [ 'image/jpeg', 'image/png', 'image/gif', 'image/jpg' ];
 
- var exts = {
-   "image/jpeg": '.jpg',
-   "image/png" : '.png'
- };
+var exts = {
+  'image/jpeg': '.jpg',
+  'image/jpg' : '.jpg',
+  'image/png' : '.png',
+  'image/gif' : '.gif'
+};
+
+var maxSize = 4 * 1024 * 1024;
 
 var uploader = multer({
   fileFilter: function(req, file, cb) {
@@ -68,7 +74,7 @@ var uploader = multer({
   }),
   limits: {
     files: 1,
-    //fileSize: 4194304
+    fileSize: maxSize
   }
 });
 
@@ -82,12 +88,52 @@ app.post('/photos/upload', uploader.single('photo'), function (req, res, next) {
   res.redirect('/uploads/original/' + req.file.filename);
 });
 
-var download = function(url, filename, callback){
-  request({
-    url: url,
-    proxy: 'http://www-cache.ujf-grenoble.fr:3128'
-  }).pipe(fs.createWriteStream(filename)).on('close', callback);
-};
+request.defaults({'proxy':'http://55.1.35.226:8080'});
+
+app.post('/photos/uploadurl', function (req, res, next) {
+  var url = req.body.url;
+  if (!url) {
+    return res.status(400).json({ message: 'Field url is required'});
+  }
+
+  request.head(url, function(err, response, body) {
+    if (err) return res.status(500).json({ message: 'Invalid URL' });
+
+    var status = response.statusCode;
+    if (status != 200 && status != 304) {
+      return res.status(400).json({ message: 'Could not access the URL' });
+    }
+
+    var type = response.headers['content-type'];
+    if(allowedMime.indexOf(type) == -1) {
+      return res.status(400).json({ message: 'Only JPEG and PNG images are accepted' });
+    }
+
+    var size = response.headers['content-length'];
+    if(size > maxSize) {
+      return res.status(400).json({ message: 'The requested image is too big' });
+    }
+
+    var ext = exts[type];
+    var id = crypto.pseudoRandomBytes(16).toString('hex');
+    var filename = 'uploads/original/' + id + ext;
+
+    request(url).pipe(fs.createWriteStream(filename)).on('close', function() {
+      res.redirect('/' + filename);
+
+      var thumbnail = 'uploads/thumbnail/' + id + '.jpg';
+      // Create thumbnail
+      sharp(filename)
+      .resize(300, 200)
+      .toFile(thumbnail, function(err) {
+        // output.jpg is a 300 pixels wide and 200 pixels high image
+        // containing a scaled and cropped version of input.jpg
+        console.log(colorThief.getPalette(thumbnail, 8));
+        // Build a color palette from an image
+      });
+    });
+  });
+});
 
 app.use("/", express.static(__dirname + "/"));
 app.use('/', express.static('views'));
